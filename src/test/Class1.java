a3a6691db9906d3b8ac5430531bd4bf28c81f1cf
Monika Zankar <monikazankar@gmail.com>
2021-08-31 07:00:30 -0700

    Branch b changes

--- src/test/Class1.java
+++ src/test/Class1.java
@@ -2,6 +2,7 @@ package test;
 
 public class Class1 {
 public static void main(String[] args) {
-	System.out.println("Hello world");
+	System.out.println("Hello world456");
+	System.out.println("New line for brnch b");
 }
 }
